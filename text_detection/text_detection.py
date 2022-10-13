c0b5b7286de1fbb988f6f87daee2c43f82a9d391
Jack Mutsers <jmutsers95@hotmail.com>
2022-10-13 11:40:42 +0200

    minor prep

--- text_detection/text_detection.py
+++ text_detection/text_detection.py
@@ -9,16 +9,11 @@ import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str,
-	help="path to input image")
-ap.add_argument("-east", "--east", type=str,
-	help="path to input EAST text detector")
-ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
-	help="minimum probability required to inspect a region")
-ap.add_argument("-w", "--width", type=int, default=320,
-	help="resized image width (should be multiple of 32)")
-ap.add_argument("-e", "--height", type=int, default=320,
-	help="resized image height (should be multiple of 32)")
+ap.add_argument("-i", "--image", type=str, help="path to input image", default="text_detection/images/test_picture.PNG")
+ap.add_argument("-east", "--east", type=str, help="path to input EAST text detector", default="text_detection/frozen_east_text_detection.pb")
+ap.add_argument("-c", "--min-confidence", type=float, default=0.5, help="minimum probability required to inspect a region")
+ap.add_argument("-w", "--width", type=int, default=320, help="resized image width (should be multiple of 32)")
+ap.add_argument("-e", "--height", type=int, default=320, help="resized image height (should be multiple of 32)")
 args = vars(ap.parse_args())
 
 # load the input image and grab the image dimensions
