ddac2c164a7ce3b4fad9582cddc6e64b2912913a
Jack Mutsers <jmutsers95@hotmail.com>
2022-10-13 11:37:27 +0200

    added visual project for personal project

---
+++ photobooth/photo_booth.py
@@ -0,0 +1,22 @@
+# import the necessary packages
+from __future__ import print_function
+from photoboothapp import PhotoBoothApp
+from imutils.video import VideoStream
+import argparse
+import time
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--output", required=False, help="path to output directory to store snapshots", default="output")
+ap.add_argument("-p", "--picamera", type=int, default=-1, help="whether or not the Raspberry Pi camera should be used")
+
+args = vars(ap.parse_args())
+
+# initialize the video stream and allow the camera sensor to warmup
+print("[INFO] warming up camera...")
+vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
+time.sleep(2.0)
+
+# start the app
+pba = PhotoBoothApp(vs, args["output"])
+pba.root.mainloop()
